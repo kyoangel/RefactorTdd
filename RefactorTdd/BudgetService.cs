4bbc834a19b5ce8ff3b5f4478d7b0db0d276176d
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2018-11-10 15:58:25 +0800

    init Test Project

---
+++ RefactorTdd/BudgetService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RefactorTdd
+{
+	public class BudgetService
+	{
+		public double TotalAmount(DateTime start, DateTime end)
+		{
+			return 0;
+		}
+	}
+}
---
+++ RefactorTddTests/BudgetServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RefactorTdd;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorTdd.Tests
+{
+	[TestClass()]
+	public class BudgetServiceTests
+	{
+		[TestMethod()]
+		public void TotalAmountTest()
+		{
+			Assert.Fail();
+		}
+	}
+}
