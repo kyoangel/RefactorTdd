5af2b1197c926bca33ac97821ff6af1f950789f4
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2018-11-10 18:13:57 +0800

    refactor

--- RefactorTdd/BudgetService.cs
+++ RefactorTdd/BudgetService.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 
 namespace RefactorTdd
@@ -20,56 +18,60 @@ namespace RefactorTdd
 
 		public double TotalAmount(DateTime start, DateTime end)
 		{
-			if (IsValidDateRange(start, end))
+			if (!IsValidDateRange(start, end))
 			{
+				return 0;
+			}
 
-				var s = start.ToString("yyyyMM");
-				var budgets = _repo.GetAll();
+			var s = start.ToString("yyyyMM");
+			var budgets = _repo.GetAll();
 
-				if (start.ToString("yyyyMM") != end.ToString("yyyyMM"))
-				{
-					DateTime tempDate = new DateTime(start.Year,start.Month,1);
-					double aggrAmount = 0;
-					do
-					{
-						var budgetByMonth =
-							budgets.SingleOrDefault(x => x.YearMonth.Equals(tempDate.ToString("yyyyMM")));
-						if (budgetByMonth != null)
-						{
-							if (tempDate.ToString("yyyyMM") == start.ToString("yyyyMM"))
-								aggrAmount += AmountPerDayInMonth(budgetByMonth, start) *
-								              (DateTime.DaysInMonth(start.Year, start.Month) - start.Day + 1);
-							else if (tempDate.ToString("yyyyMM") == end.ToString("yyyyMM"))
-								aggrAmount += AmountPerDayInMonth(budgetByMonth, end) * end.Day;
-							else
-								aggrAmount += budgetByMonth.Amount;
-						}
-
-						tempDate = tempDate.AddMonths(1);
-					} while (tempDate.Year <= end.Year && tempDate.Month <= end.Month);
-
-					return aggrAmount;
-				}
-
+			if (IsSameMonth(start, end))
+			{
 				var budget = budgets.SingleOrDefault(x => x.YearMonth.Equals(s));
 				if (budget == null)
 				{
 					return 0;
 				}
+
 				var budgetPerDay = budget.Amount / DateTime.DaysInMonth(start.Year, start.Month);
 				return budgetPerDay * DaysInterval(start, end);
 			}
 			else
 			{
-				return 0;
+				DateTime tempDate = new DateTime(start.Year, start.Month, 1);
+				double aggrAmount = 0;
+				do
+				{
+					var budgetByMonth =
+						budgets.SingleOrDefault(x => x.YearMonth.Equals(tempDate.ToString("yyyyMM")));
+					if (budgetByMonth != null)
+					{
+						if (tempDate.ToString("yyyyMM") == start.ToString("yyyyMM"))
+							aggrAmount += AmountPerDayInMonth(budgetByMonth, start) *
+							              (DateTime.DaysInMonth(start.Year, start.Month) - start.Day + 1);
+						else if (tempDate.ToString("yyyyMM") == end.ToString("yyyyMM"))
+							aggrAmount += AmountPerDayInMonth(budgetByMonth, end) * end.Day;
+						else
+							aggrAmount += budgetByMonth.Amount;
+					}
+
+					tempDate = tempDate.AddMonths(1);
+				} while (tempDate <= end);
+
+				return aggrAmount;
 			}
+		}
+
+		private static bool IsSameMonth(DateTime start, DateTime end)
+		{
+			return start.ToString("yyyyMM") == end.ToString("yyyyMM");
 		}
 
 		private static int AmountPerDayInMonth(Budget budgetByMonth, DateTime tempDate)
 		{
 			return budgetByMonth.Amount / DateTime.DaysInMonth(tempDate.Year, tempDate.Month);
 		}
-
 
 		private static bool IsValidDateRange(DateTime start, DateTime end)
 		{
